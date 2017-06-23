8ceae2e994eaff5399dcfd337ffbb7211ee15394
“liyuli” <1948538139@qq.com>
2017-06-23 18:22:45 +0800

    添加工程GitGit

---
+++ GitGit/GitGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitGit
+//
+//  Created by 玉立 on 17/6/23.
+//  Copyright © 2017年 YuLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
